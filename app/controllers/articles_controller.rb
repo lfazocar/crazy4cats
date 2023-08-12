d7804b0104647ab2f866aad186da848db06d32e9
Felipe <lfazocar@users.noreply.github.com>
2023-08-12 01:08:00 -0400

    feat: Added Comment and Reaction as members of Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,12 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
 
+  # Role permissions
+  before_action :authenticate_user!, except: %i[ index show ]
+  before_action only: %i[ edit update destroy] do
+    authorize_request(["admin"])
+  end
+
   # GET /articles or /articles.json
   def index
     @articles = Article.all
@@ -8,6 +14,19 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1 or /articles/1.json
   def show
+    @comments = Comment.where(article_id: params[:id]).order(:created_at)
+
+    reactions = Reaction.where(article_id: params[:id])
+    reaction_type_count = Hash.new(0)
+    reactions.pluck(:reaction_type).each do |reaction|
+      reaction_type_count[reaction] += 1
+    end
+    @likes = reaction_type_count["like"]
+    @dislikes = reaction_type_count["dislike"]
+
+    if user_signed_in?
+      @user_reaction = reactions.pluck(:user_id, :reaction_type).select{ |ele| ele[0] == current_user.id }.dig(0,1)
+    end
   end
 
   # GET /articles/new
@@ -17,6 +36,17 @@ class ArticlesController < ApplicationController
 
   # GET /articles/1/edit
   def edit
+  end
+
+  # POST /articles/1
+  def comment
+    @comment = Comment.create(comment_params.merge(user_id: current_user.id, article_id: params[:id]))
+    redirect_to article_path
+  end
+
+  def reaction
+    @reaction_type = Reaction.create(reaction_params.merge(user_id: current_user.id, article_id: params[:id]))
+    redirect_to article_path
   end
 
   # POST /articles or /articles.json
@@ -66,5 +96,13 @@ class ArticlesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title, :content, :image, :user_id)
+    end
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+
+    def reaction_params
+      params.permit(:reaction_type)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :articles
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :articles do
+    post 'comment', on: :member
+    post 'reaction', on: :member
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+    devise_for :users
+
+    root "articles#index"
 end
